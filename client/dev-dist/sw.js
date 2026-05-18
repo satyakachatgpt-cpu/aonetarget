676810757ca5629f8444bf74ddb0b024ccff5735
Sumit-8287 <sumitbudhwar74@gmail.com>
2026-05-18 11:32:51 +0530

    update splash screen on laptop remove

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-cd40d911'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.a0v65ep406"
+    "revision": "0.mdvr72ev9l"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
