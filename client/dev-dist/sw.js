d2e8ff043a5c115f5c8bd558d5c41c81bb12463a
Sumit-8287 <sumitbudhwar74@gmail.com>
2026-05-25 12:55:38 +0530

    update jab net slow ho to icon k name show ho rhe the vo fix kr deya

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-cd40d911'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.i7eg40cs118"
+    "revision": "0.jfvjjlu6qfo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
