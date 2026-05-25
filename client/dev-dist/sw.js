7bd2437cb353da87cf7084fcbd99225505fefaf3
Sumit-8287 <sumitbudhwar74@gmail.com>
2026-05-25 16:01:58 +0530

    update android uper ka space correct

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-cd40d911'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.mqk5oi412j8"
+    "revision": "0.g7l6arvuo5o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
