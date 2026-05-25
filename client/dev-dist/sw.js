64a4f7ac04152fce59349af9f6a1e02e6878a23c
Sumit-8287 <sumitbudhwar74@gmail.com>
2026-05-25 12:29:22 +0530

    update shuffle process correct form after shuffle test auto not open

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-cd40d911'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.5fjpgb1l2ig"
+    "revision": "0.8vm3i7gsgk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
