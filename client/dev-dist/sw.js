e32f880943598c74dea0aa7db535cac378d1e60b
Sumit-8287 <sumitbudhwar74@gmail.com>
2026-05-27 16:57:20 +0530

    update test swipe submit

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-cd40d911'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.n9ltog2pvb8"
+    "revision": "0.0pm0qafkb5o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
