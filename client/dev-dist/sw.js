2a00acfbb16dc766f921903de5dcf34182207044
Sumit-8287 <sumitbudhwar74@gmail.com>
2026-05-28 12:09:34 +0530

    update security right click disable ss stop copy text not allowed f12 not working

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-cd40d911'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.qtf9pkeaq8g"
+    "revision": "0.sdpmotqh5n"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
