6eaa5b42a98966222796aa3fa35a58a495d6329a
Sumit-8287 <sumitbudhwar74@gmail.com>
2026-06-30 13:02:04 +0530

    update explore page search baar fix

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-df1cb600'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.kodgg0dnc18"
+    "revision": "0.5jrr4fkqra8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
